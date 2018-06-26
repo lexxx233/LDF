506d2c3fc2f028d2643bbe45962d29f1dee6b0c9
lexxx233 <lexxx233@umn.edu>
2018-06-26 14:35:14 -0400

    Added Longitudinal Variable and TType (TemporalType)

---
+++ ldf/LongitudinalVariable.py
@@ -0,0 +1,42 @@
+import pandas as pd
+
+
+class LongitudinalVariable:
+    def __init__(self, ivar=None):
+        self.data = pd.DataFrame(ivar, columns=list('td'))
+
+    def append(self, ivar):
+        self.data = self.data.append(pd.DataFrame(ivar, columns=list('td')))
+
+    def remove_t(self, tvars):
+        '''
+
+        :param tvars: array of time t
+        :return:
+        '''
+        self.data = self.data.drop(self.findarray(self.data.loc[:, 't'].tolist(), tvars))
+
+    def select_range(self, tstart, tend):
+        return self.data[(self.data[:, 't'] >= tstart) & (self.data[:, 't'] <= tend)]
+
+    def t(self):
+        return self.data.loc[:, 't'].tolist()
+
+    def d(self):
+        return self.data.loc[:, 'd'].tolist()
+
+    @staticmethod
+    def find(ilist, ivar):
+        try:
+            return ilist.index(ivar)
+        except ValueError:
+            return -1
+
+    @staticmethod
+    def findarray(ilist, ivars):
+        res = []
+        for i in ivars:
+            j = LongitudinalVariable.find(ilist, i)
+            if j != -1:
+                res.append(j)
+        return res
---
+++ ldf/TType.py
@@ -0,0 +1,6 @@
+from enum import Enum
+
+
+class TType(Enum):
+    FORMATED = 0  # Default 2016-09-19:18:00:00
+    REAL = 1  # Start at 0 - 0.2 - 1 - 3.2 - 3.03 - and so on
